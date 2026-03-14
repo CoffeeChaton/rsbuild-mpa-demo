0bade3625526f890d7ab07931b4dc5fa59294398
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2026-03-15 02:49:21 +0800

    fix: script

--- src/pages/game2/index.tsx
+++ src/pages/game2/index.tsx
@@ -4,6 +4,7 @@ import { ArsenalLayout } from "./layout/ArsenalLayout";
 export const App = () => {
   return (
     <>
+      {/* Navbar is Top w-100vw && h-[100px] */}
       <Navbar />
       <ArsenalLayout />;
     </>
