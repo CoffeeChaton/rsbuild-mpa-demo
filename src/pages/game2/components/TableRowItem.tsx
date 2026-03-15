e416ddd1ededa8579e555a9528472877ecd658a9
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2026-03-15 11:20:56 +0800

    refactor(/game2/table-row): extract RowInputs component

--- src/pages/game2/components/TableRowItem.tsx
+++ src/pages/game2/components/TableRowItem.tsx
@@ -1,9 +1,9 @@
 import React, { memo, useCallback } from "react";
-import { Checkbox, Flex, Table, Text, TextField } from "@radix-ui/themes";
+import { Checkbox, Table } from "@radix-ui/themes";
 import type { IItem, IRowResult } from "../types";
-import { RaritySelect } from "./RaritySelect";
 import { StatCell } from "./TableRowItem/StatCell";
 import { RowActions } from "./TableRowItem/RowActions";
+import { RowInputs } from "./TableRowItem/RowInputs";
 
 const areRowValuesEqual = (prev: IRowResult, next: IRowResult): boolean => (
   prev.costMoney === next.costMoney
@@ -58,45 +58,11 @@ const TableRowItemComponent: React.FC<ITableRowItemProps> = ({ item, row, index,
           onCheckedChange={v => handleUpdate("calculate", !!v)}
         />
       </Table.Cell>
-      <Table.Cell>
-        {/* 選擇 星級 / 稀有度 */}
-        <RaritySelect
-          value={item.rarity}
-          onValueChange={val => handleUpdate("rarity", Number(val))}
-        />
-      </Table.Cell>
-      <Table.Cell>
-        <TextField.Root
-          size="1"
-          value={item.name}
-          onChange={e => handleUpdate("name", e.currentTarget.value)}
-        />
-      </Table.Cell>
-      <Table.Cell>
-        <TextField.Root
-          size="1"
-          variant="soft"
-          value={item.note}
-          onChange={e => handleUpdate("note", e.target.value)}
-        />
-      </Table.Cell>
-      <Table.Cell>
-        {/* 模組 */}
-        <Flex align="center" gap="1">
-          <TextField.Root type="number" size="1" style={{ width: 28 }} value={item.moduleFrom} onChange={e => handleUpdate("moduleFrom", e.target.value)} />
-          <Text size="1" color="gray">→</Text>
-          <TextField.Root type="number" size="1" style={{ width: 28 }} value={item.moduleTo} onChange={e => handleUpdate("moduleTo", e.target.value)} />
-        </Flex>
-      </Table.Cell>
-      <Table.Cell>
-        <Flex align="center" gap="1">
-          <TextField.Root type="number" size="1" style={{ width: 28 }} value={item.e1} onChange={e => handleUpdate("e1", e.target.value)} />
-          <TextField.Root type="number" size="1" style={{ width: 32 }} value={item.l1} onChange={e => handleUpdate("l1", e.target.value)} />
-          <Text size="1" color="gray">→</Text>
-          <TextField.Root type="number" size="1" style={{ width: 28 }} value={item.e2} onChange={e => handleUpdate("e2", e.target.value)} />
-          <TextField.Root type="number" size="1" style={{ width: 32 }} value={item.l2} onChange={e => handleUpdate("l2", e.target.value)} />
-        </Flex>
-      </Table.Cell>
+      {/* 中間計算區域 */}
+      <RowInputs
+        item={item}
+        onUpdate={onUpdate}
+      />
       <StatCell value={row.costMoney} tone="money" variant="cost" />
       <StatCell value={row.costBooks} tone="books" variant="cost" />
       <StatCell value={row.cumMoney} tone="money" variant="cum" status={row.moneyStatus} showValue={item.calculate} />
