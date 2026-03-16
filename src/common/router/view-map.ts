dc9d585c29d1734a80a4bcfc1095421ec046891d
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2026-03-16 15:27:37 +0800

    chore: use webpackChunkName

--- src/common/router/view-map.ts
+++ src/common/router/view-map.ts
@@ -19,10 +19,11 @@ export const VIEW_MAP: Record<
 	Exclude<TPageKey, "404">,
 	LazyExoticComponent<React.FC>
 > = {
-	index: lazyPage(() => import("../../pages/index/index")),
-	products: lazyPage(() => import("../../pages/products/index")),
-	"map-edit": lazyPage(() => import("../../pages/map-edit/index")),
-	game: lazyPage(() => import("../../pages/game/index")),
-	game2: lazyPage(() => import("../../pages/game2/index")),
-	game3: lazyPage(() => import("../../pages/game3/index")),
+	// 使用 Magic Comments 賦予檔案有意義的名字
+	index: lazyPage(() => import(/* webpackChunkName: "p-index" */ "../../pages/index/index")),
+	products: lazyPage(() => import(/* webpackChunkName: "p-products" */ "../../pages/products/index")),
+	"map-edit": lazyPage(() => import(/* webpackChunkName: "p-map-edit" */ "../../pages/map-edit/index")),
+	game: lazyPage(() => import(/* webpackChunkName: "p-game" */ "../../pages/game/index")),
+	game2: lazyPage(() => import(/* webpackChunkName: "p-game2" */ "../../pages/game2/index")),
+	game3: lazyPage(() => import(/* webpackChunkName: "p-game3" */ "../../pages/game3/index")),
 };
